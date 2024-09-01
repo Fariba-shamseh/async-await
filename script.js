b96161a01d75d9c878b8f0d3039086f445a41d12
Fariba Shamseh <fariba.shamseh0@gmail.com>
2024-09-01 12:50:24 +0430

    async-await

---
+++ script.js
@@ -0,0 +1,61 @@
+'use strict';
+
+//func renderCountry----------------------------------------------------------------
+const renderCountry = function (data, className = '') {
+  const html = `
+  <article class="country ${className}">
+  <img class="country__img" src="${data.flags.png}" />
+  <div class="country__data">
+    <h3 class="country__name">${data.name.common}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${(
+      +data.population / 1000000
+    ).toFixed(1)}</p>
+    <p class="country__row"><span>🗣️</span>${Object.values(data.languages).join(
+      ', '
+    )}</p>
+    <p class="country__row"><span>💰</span>${
+      data.currencies[Object.keys(data.currencies)[0]].name
+    }</p>
+  </div>
+</article>
+`;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+//---------------------------------------------------------------------
+const getPosition = function () {
+  return new Promise(function (resolve, reject) {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+};
+//---------------------------------------------------------------------
+
+// fetch(`https://restcountries.com/v3.1/name/${country}`).then(res =>
+//   console.log(res)
+// );
+
+const key = '908799934974028841283x8131';
+//Geolocation
+const whereAmI = async function (country) {
+  const pos = await getPosition();
+  const { latitude: lat, longitude: lng } = pos.coords;
+
+  //reverse geocoding
+  const resGeo = await fetch(
+    `https://geocode.xyz/${lat},${lng}?json=1=xml&auth=${key}`
+  );
+  const dataGeo = await resGeo.json();
+  console.log(dataGeo);
+
+  //country data
+  const res = await fetch(
+    `https://restcountries.com/v3.1/name/${dataGeo.country}`
+  );
+  const data = await res.json();
+  console.log(data);
+  renderCountry(data[0]);
+};
+whereAmI('germany');
+console.log('first');
